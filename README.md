5f6c66fd6e4b1e0ebd2b8fe46989b28dbbca673f
root <root@ip-10-1-20-96.ec2.internal>
2019-10-24 17:31:41 +0000

    Adding docker & compose file for lawline dev

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# docker-dev
+# docker develepment enviornment for lawline application
+
+#Lawline Dev enroviment setup on local ubnutu machine
+
+ - Checkout of git branch on your machine 
+    git clone https://github.com/petabloc/docker-dev.git
+
+ - Intsall leteast version of docker and docker-compose 
+   bash dockerInstllation.sh
+
+ - Checkout of Dev-ui and Dev-api code in current repo
+
+ - Run docker compose command
+
+   - Execute the --build command for first time or when you have change in docker/compose file
+     docker-compose up -d --build 
+   - Create the container 
+     docker-compose up -d
+
+
+
+
+
---
+++ dockerInstllation.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get install docker-ce -y
+sudo groupadd docker
+sudo usermod -aG docker vagrant
+sudo systemctl enable docker
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
